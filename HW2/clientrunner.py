275ce716e09d114c1771e19d5da5489adf18ea53
Keith Skinner <brian.skinner121@myci.csuci.edu>
2019-09-30 04:39:39 -0700

    basic client runner

--- HW2/clientrunner.py
+++ HW2/clientrunner.py
@@ -8,4 +8,15 @@ POST = 8080
 SPACING = '\n=\n'*20
 
 def connectToServer(host, port):
+    try:
+        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.connect((host, port))
+        logging.info('Connect to server: ' + host + ' on port: ' + str(port))
+        return socket
+    except:
+        return None
+
+if __name__ == '__main__':
+    print('Welcome to TicTackToe\n')
+    print(SPACING)
     pass
