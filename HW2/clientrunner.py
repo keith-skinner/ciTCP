25b3db387364c5ef7d2eb91da46628bae98d6b9b
Keith Skinner <brian.skinner121@myci.csuci.edu>
2019-09-30 04:38:52 -0700

    adding client runner

---
+++ HW2/clientrunner.py
@@ -0,0 +1,11 @@
+import argparse, socket, logging
+from client import Client
+from engine import Engine
+
+HOST = '127.0.0.1'
+POST = 8080
+
+SPACING = '\n=\n'*20
+
+def connectToServer(host, port):
+    pass
