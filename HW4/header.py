a48ef6edffa57553f3a6c7bed17b00d7b306aef0
Keith <brian.skinner121@myci.csuci.edu>
2019-11-06 07:22:34 -0800

    added copy "constructor" to header class

--- HW4/header.py
+++ HW4/header.py
@@ -1,12 +1,22 @@
 import sys
 
 class Header:
-    def __init__(self):
+    def __init__(self, seq=0, ack=0, window=0, checksum=0, flags=0):
         self.seq = 0
         self.ack = 0
-        self.window = 0
+        self.window = 0 
         self.checksum = 0
         self.flags = 0
+
+    def copy(self):
+        h = Header(
+            self.seq, 
+            self.ack, 
+            self.window, 
+            self.checksum,
+            self.flags
+        )
+        return h
 
     def resetFlags(self):
         self.flags = 0
@@ -38,13 +48,13 @@ class Header:
     
     def setWindow(self, window):
         if window < 0 or window >= 2**16:
-            raise ValueError("window ({}) must be in [0, 2**16)".format(number))
+            raise ValueError("window ({}) must be in [0, 2**16)".format(window))
         self.window = window
         return self
 
     def setChecksum(self, checksum):
         if checksum < 0 or checksum >= 2**16:
-            raise ValueError("checksum ({}) must be in [0, 2**16)".format(number))
+            raise ValueError("checksum ({}) must be in [0, 2**16)".format(checksum))
         self.checksum = checksum
         return self
 
@@ -85,3 +95,6 @@ class Header:
         s += '|{:^9}|{}|{}|{}|\n'.format(U, A, S, F)
         s += '+-+-+-+-+-+-+-+-+'
         return s
+
+    def __len__(self):
+        return 9 #number of bytes
